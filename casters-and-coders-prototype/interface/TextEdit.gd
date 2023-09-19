04ad5ee4fa43c6d9d96392bcea94a090d56c174e
Brennan Seymour <brenno263@gmail.com>
2023-09-18 23:22:44 -0500

    add green pixel UI theme & build reactive code editor UI with Containers

---
+++ casters-and-coders-prototype/interface/TextEdit.gd
@@ -0,0 +1,17 @@
+extends TextEdit
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	add_keyword_color("foo", Color(1, 0, 0))
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
