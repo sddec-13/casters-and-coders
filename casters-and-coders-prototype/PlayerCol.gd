0495141d4f2260298eaabd064566d3deecd8d722
Branden Butler <bwtbutler@hotmail.com>
2023-09-29 10:12:04 -0500

    Revert modification of scene to prep for merge

--- casters-and-coders-prototype/PlayerCol.gd
+++ casters-and-coders-prototype/PlayerCol.gd
@@ -1,23 +1,8 @@
 extends KinematicBody2D
 
-signal event_happened
-signal event_with_args
+
 var speed = 250
 var velocity = Vector2()
-
-func _ready():
-	# Programmatically add signal connection, UI works too
-	#self.get_node("Camera2D").connect("script_finished_executing", self, "_on_Camera2D_script_finished_executing")
-	
-	# Call a method manually, not using signals. Python-side method
-	# then emits a signal that is routed back here. Appears to all be done on a single thread
-	self.get_node("Camera2D").exec_script("test_script.py")
-	# Call method using signals
-	# unsure if the signal processing is done on a separate thread in GDScript
-	emit_signal("event_happened")
-	
-	# Call exec_script using a signal
-	emit_signal("event_with_args", "some_script")
 
 func get_input():
 	# Detect up/down/left/right keystate and only move when pressed.
@@ -43,8 +28,3 @@ func _physics_process(delta):
 	$AnimatedSprite.play()
 	
 	move_and_slide(velocity)
-
-# Handler for the signal sent by Python with arguments,
-# handlers with no-args work too
-func _on_Camera2D_script_finished_executing(script_name):
-	print("Finished executing script with name " + script_name) # Replace with function body.
---
+++ casters-and-coders-prototype/signal_example.gd
@@ -0,0 +1,29 @@
+extends Node
+
+signal event_happened
+signal event_with_args
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+func _ready():
+	# Programmatically add signal connection, UI works too
+	#self.get_node("Camera2D").connect("script_finished_executing", self, "_on_Camera2D_script_finished_executing")
+	
+	# Call a method manually, not using signals. Python-side method
+	# then emits a signal that is routed back here. Appears to all be done on a single thread
+	self.get_node("Camera2D").exec_script("test_script.py")
+	# Call method using signals
+	# unsure if the signal processing is done on a separate thread in GDScript
+	emit_signal("event_happened")
+	
+	# Call exec_script using a signal
+	emit_signal("event_with_args", "some_script")
+
+
+# Handler for the signal sent by Python with arguments,
+# handlers with no-args work too
+func _on_Camera2D_script_finished_executing(script_name):
+	print("Finished executing script with name " + script_name) # Replace with function body.
