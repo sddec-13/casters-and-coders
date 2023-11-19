674ddec4ed8911a8c69a2fcb197d4d6618ad439a
Brennan Seymour <brenno263@gmail.com>
2023-11-19 16:33:21 -0600

    built out python interpreter as autoloaded node

---
+++ casters-and-coders-prototype/python_engine/puzzle_loader.gd
@@ -0,0 +1,33 @@
+extends Node
+
+func load_definition_string(name: String) -> String:
+	var f = File.new()
+	var path = "res://puzzle_defs/%s.json" % name
+	var err = f.open(path, File.READ)
+	if err != OK:
+		print("Failed to load definition for " + path)
+		return ""
+	var text = f.get_as_text()
+	f.close()
+	return text
+	
+func load_definition(name: String):
+	var def_string = load_definition_string(name)
+	var parse = JSON.parse(def_string)
+	if parse.error != OK:
+		print("error parsing JSON definition for ", name)
+		print(parse.error_string, parse.error_line)
+
+func load_source(name: String) -> String:
+	var f = File.new()
+	var path = "user://puzzle_scripts/%s.py" % name
+	# If the file doesn't exist yet, just run an empty file.
+	if not f.file_exists(path):
+		return ""
+	var err = f.open(path, File.READ)
+	if err != OK:
+		print("Failed to load source for " + path)
+		return ""
+	var source = f.get_as_text()
+	f.close()
+	return source
