a71163d3b76b4784ae7377e087e84327b2aab7c3
Brennan Seymour <brenno263@gmail.com>
2023-11-28 21:53:05 -0600

    integrate pedestal and code editor with scripting engine and assemble a demo scene

---
+++ casters-and-coders-prototype/scripts/script_pedestal.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+export var puzzle_name: String
+onready var interactable = $Interactable
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	interactable.connect("interacted", self, "_interacted")
+	PythonManager.load_puzzle(puzzle_name)
+
+func _interacted():
+	PythonManager.clear()
+	MenuManager.open_code_editor(puzzle_name)
+	MenuManager.connect("menu_closed", self, "_editor_closed")
